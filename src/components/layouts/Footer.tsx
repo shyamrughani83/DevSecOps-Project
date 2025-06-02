79bfef87474bc9a8af7ae47a662a0fd42b1bdc64
shyamrughani83 <shyam.rughani@siliconithub.com>
2025-06-02 16:32:18 +0530

    updated in footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
       <Box>
         <Divider>
           <Typography color="grey.700" variant="h6">
-            Developed by{" "}
+            Developed by Shyam{" "}
             <Link
               href="https://github.com/crazy-man22"
               underline="none"
