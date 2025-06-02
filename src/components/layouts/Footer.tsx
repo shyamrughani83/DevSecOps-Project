5276e432c6ebdfe4413c7c1ba3c2f082a0431e63
shyamrughani83 <shyam.rughani@siliconithub.com>
2025-06-02 16:24:27 +0530

    updated footer name

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -29,7 +29,7 @@ export default function Footer() {
               sx={{ color: "text.primary" }}
               target="_blank"
             >
-              Crazy Man
+              Cloud Man SR
             </Link>
           </Typography>
         </Divider>
